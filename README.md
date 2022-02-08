cbd3bbfad7cb30ab7bc6207b2b3aa8a0a6eb73c2
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2022-02-08 16:27:54 -0500

    created a simple api endpoint

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webservice
 a web api application
+
+
+### Prerequisites:
+1) Go v1.17
+
+
+### How to deploy:
+1) Clone the repository
+2) Run the command <code>go run server.go</code> in the terminal from the repository directory
---
+++ server_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthz", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
