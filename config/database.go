4a0e11c14e3f47e4f80f13a198a0b53655b2b3ca
Olayinka Olasunkanmi <sj.olayinka@gmail.com>
2022-02-16 10:53:28 -0500

    removed panic when unable to connect to db

--- config/database.go
+++ config/database.go
@@ -25,7 +25,7 @@ func SetupDatabaseConnection() *gorm.DB {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to create a connection to database")
+		fmt.Println("Failed to create a connection to database")
 	}
 	db.AutoMigrate(&entity.User{})
 	return db
@@ -34,7 +34,7 @@ func SetupDatabaseConnection() *gorm.DB {
 func CloseDatabaseConnection(db *gorm.DB) {
 	dbSQL, err := db.DB()
 	if err != nil {
-		panic("Failed to close connection from database")
+		fmt.Println("Failed to close connection from database")
 	}
 	dbSQL.Close()
 }
